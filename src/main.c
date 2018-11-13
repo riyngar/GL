6de8e74cda10547a48e5f79398fe5ad542e5d192
rakul iyngarathasan <rakul.iyngarathasan@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:27:55 +0100

    correction_dsexo2

--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+/*---------plage de valeurs type  -----------*/
+// char    <  short int       <     long
+// 1octect |  2 octets(16bit) |  4octets
+// unsigned short int [0(2^16-1)]         
+
+
+
+
 #include "my_header.h"
+#include <math.h>
+
+#define ECART 1e-10
 
 int main(void)
 {
+    double a = 1.0, b = 2.0, c = 3.0;
+    double delta;
+    unsigned char nb_solutions;
+
+    delta = b*b - 4.0 * a *c;
+
+    if ( fabs(delta) <  ECART )
+        nb_solutions = 1;
+    else if( delta > 0)
+        nb_solutions = 2;
+    else
+        nb_solutions = 0;
+
     return 0;
 }
