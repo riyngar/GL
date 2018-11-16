352826e8cfcdda803f3537709f776f6f1d70f168
riyngar <rakul.iyngarathasan@u-psud.fr>
2018-11-16 14:56:05 +0100

    exi 44 fini

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <ansi_c.h>
+#include <userint.h>
+#include "TP_lib.h"
+#include "cvi_gui.h"
 
-#include "my_header.h"
-
-int main(void)
+void main(void)
 {
-    return 0;
+	short resultat, i=0;
+	while(i<10)
+	{
+		resultat= LancerDe();
+		 printf("numero %d ",resultat) ;
+		i++;
+	}
 }
+//yo30
