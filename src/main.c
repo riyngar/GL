19a09bb34a5a3f3aa412bae389c116bdd0e94539
riyngar <rakul.iyngarathasan@u-psud.fr>
2018-11-27 20:08:46 +0100

    lol

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-//#include "my_header.h"
+#include "TP_lib_suite.h"
 
 int main(void)
 {
 	unsigned char i,k,gain=0;
+	
+	 void initialiserTirage();
+	short tirerNumero();
 	unsigned char gagnante[6]= {20,25,1,2,4,6};
 	unsigned char joueur[6]={6,5,2,12,4,20};
+
+	 
 	
-	 for (i=0;i<6;i++)
+	
+	
+	for (i=0;i<6;i++)
 	 {
 		 for (k=0;k<6;k++)
 		{
