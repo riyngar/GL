fdb4efbb33aecea83be17a59903b9753a9f93e6d
rakul iyngarathasan <rakul.iyngarathasan@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:45:52 +0100

    partie2

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+//#include "my_header.h"
 
 int main(void)
 {
-    return 0;
+	unsigned char i,k,gain=0;
+	unsigned char gagnante[6]= {20,25,12,2,4,6};
+	unsigned char joueur[6]={6,25,2,12,4,20};
+	
+	 for (i=0;i<6;i++)
+	 {
+		 for (k=0;k<6;k++)
+		{
+			if(gagnante[i] == joueur[k])
+			{
+				gain++;
+				
+			}
+		}
+		
+	 }
+	
+  int debug=0; 
 }
