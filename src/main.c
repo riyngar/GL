665d0b07f53eace0b2d596ef9899457c376f753d
rakul iyngarathasan <rakul.iyngarathasan@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:37:29 +0100

    fini

--- src/main.c
+++ src/main.c
@@ -1,9 +1,43 @@
+#include <userint.h>
+#include <ansi_c.h>
+#include "stdlib.h"
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
 
-int main(void)
+
+void main(void)
 {
-    return 0;
+  
+	char prenom[20];
+	char nom[100];
+	int annee;
+	int actuel;
+	int age ;
+	char phrase[100];
+	//char phrase[100]=" je m'appelle Rakul Iyngarathasan et j'ai 19 ans";
+	
+	
+	
+	printf("prenom?\n");
+	scanf ("%s",prenom);
+	
+	printf("nom?\n");
+	scanf ("%s",nom);
+	
+	printf("annee de naissance ?\n");
+	scanf ("%d",&annee);
+	
+	printf("annee actuel?\n");
+	scanf ("%d",&actuel);
+	
+	 age = actuel-annee;
+	
+	sprintf(phrase," je m'appelle %s %s et j'ai %d ans", prenom,nom,age);
+	
+	MessagePopup ("identié", phrase);
+	
+	
+	
+	int a=0;
 }
